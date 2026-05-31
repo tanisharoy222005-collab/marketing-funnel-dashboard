7a4452baf6760ca4cd84ae740073315872189bb3
tanisharoy222005-collab <tanisharoy222005@gmail.com>
2026-05-31 22:43:10 +0530

    Update funnel_dropoff.sql

--- sql/funnel_dropoff.sql
+++ sql/funnel_dropoff.sql
@@ -1,17 +1,21 @@
--- Funnel Progression Analysis
+WITH funnel AS (
 
 SELECT
-event_name,
-COUNT(*) AS users
-FROM analytics.events
-WHERE event_name IN (
-'page_view',
-'cta_click',
-'form_start',
-'generate_lead'
-)
-GROUP BY event_name;
+  COUNTIF(event_name='session_start') AS sessions,
+  COUNTIF(event_name='user_engagement') AS engaged_sessions,
+  COUNTIF(event_name='cta_click') AS cta_clicks,
+  COUNTIF(event_name='form_start') AS form_starts,
+  COUNTIF(event_name='generate_lead') AS leads
 
--- So What:
--- Measures user progression through the funnel.
--- Identifies conversion bottlenecks.
+FROM
+  `project.analytics.events_*`
+
+)
+
+SELECT
+  *,
+  ROUND((engaged_sessions/sessions)*100,2) AS engagement_rate,
+  ROUND((cta_clicks/sessions)*100,2) AS cta_rate,
+  ROUND((form_starts/sessions)*100,2) AS form_start_rate,
+  ROUND((leads/sessions)*100,2) AS lead_conversion_rate
+FROM funnel;
