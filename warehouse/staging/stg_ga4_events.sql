3f8ae0f5cbe1479961dcffe3281e5915a7e6c7cd
tanisharoy222005-collab <tanisharoy222005@gmail.com>
2026-05-31 23:11:30 +0530

    Create stg_ga4_events.sql

---
+++ warehouse/staging/stg_ga4_events.sql
@@ -0,0 +1,21 @@
+/*
+Staging Layer
+GA4 Events
+*/
+
+SELECT
+
+date,
+source,
+medium,
+campaign,
+landing_page,
+
+users,
+sessions,
+engaged_sessions,
+cta_clicks,
+form_starts,
+leads
+
+FROM ga4_sample;
