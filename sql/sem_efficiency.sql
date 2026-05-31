733b22f8da622b851f3dea56dfad9cbea1a9b136
tanisharoy222005-collab <tanisharoy222005@gmail.com>
2026-05-31 22:44:27 +0530

    Update sem_efficiency.sql

--- sql/sem_efficiency.sql
+++ sql/sem_efficiency.sql
@@ -1,13 +1,12 @@
--- Paid Search Efficiency
-
 SELECT
-campaign_name,
-SUM(cost) AS spend,
-SUM(conversions) AS conversions,
-SUM(cost)/NULLIF(SUM(conversions),0) AS cost_per_conversion
-FROM ads_data
-GROUP BY campaign_name;
-
--- So What:
--- Measures campaign profitability.
--- Identifies opportunities to improve ROI.
+  campaign_name,
+  SUM(cost) AS spend,
+  SUM(clicks) AS clicks,
+  SUM(conversions) AS leads,
+  ROUND(SUM(cost)/SUM(conversions),2) AS cost_per_lead
+FROM
+  `project.marketing.google_ads_campaigns`
+GROUP BY
+  campaign_name
+ORDER BY
+  cost_per_lead ASC;
